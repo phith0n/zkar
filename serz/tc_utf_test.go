a6db31ef22eb183223c237bdeb63a5dac1ee422d
phith0n <phith0n.ph2f@gmail.com>
2024-03-14 18:50:22 +0800

    improve example test

--- serz/tc_utf_test.go
+++ serz/tc_utf_test.go
@@ -7,6 +7,31 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
+func TestCC6WithOverlongEncoding(t *testing.T) {
+	// read original CommonsCollections6 payload
+	data, err := os.ReadFile("../testcases/ysoserial/CommonsCollections6.ser")
+	require.NoError(t, err)
+	ser, err := FromBytes(data)
+	require.NoError(t, err)
+
+	// enable UTF-8 overlong encoding feature
+	err = ser.Walk(func(obj Object) error {
+		if u, ok := obj.(*TCUtf); ok {
+			u.SetOverlongSize(OverlongEncodingTwoBytes)
+		}
+
+		return nil
+	})
+	require.NoError(t, err)
+
+	// generate new payload
+	data2 := ser.ToBytes()
+
+	// read new payload
+	_, err = FromBytes(data2)
+	require.NoError(t, err)
+}
+
 func TestToOverlongEncoding(t *testing.T) {
 	require.Equal(t, []byte("\xC0\xAE"), toOverlongEncoding([]byte("."), OverlongEncodingTwoBytes))
 	require.Equal(t, []byte("\xE0\x80\xAE"), toOverlongEncoding([]byte("."), OverlongEncodingThreeBytes))
@@ -22,25 +47,4 @@ func TestToOverlongEncoding(t *testing.T) {
 	require.Equal(t, []byte{0xC0, 0xFF}, fromOverlongEncoding([]byte{0xC0, 0xFF}))
 	require.Equal(t, []byte{0xE0, 0xAE, 0x38}, fromOverlongEncoding([]byte{0xE0, 0xAE, 0x38}))
 	require.Equal(t, []byte("org.example.Evil"), fromOverlongEncoding([]byte("org.example.Evil")))
-}
-
-func TestCC6WithOverlongEncoding(t *testing.T) {
-	data, err := os.ReadFile("/home/owen/app/ysoserial/cc6.ser")
-	require.NoError(t, err)
-
-	ser, err := FromBytes(data)
-
-	require.NoError(t, err)
-
-	err = ser.Walk(func(obj Object) error {
-		if u, ok := obj.(*TCUtf); ok {
-			u.SetOverlongSize(OverlongEncodingTwoBytes)
-		}
-
-		return nil
-	})
-	require.NoError(t, err)
-
-	err = os.WriteFile("/home/owen/app/ysoserial/cc6-2.ser", ser.ToBytes(), 0o644)
-	require.NoError(t, err)
 }
