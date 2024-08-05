5309fb4d83854ef31d49d226ba9e58e3d8e80dc8
phith0n <phith0n.ph2f@gmail.com>
2024-08-05 15:36:36 +0800

    added some unittest for stream

--- commons/stream_test.go
+++ commons/stream_test.go
@@ -1,6 +1,7 @@
 package commons
 
 import (
+	"bytes"
 	"io"
 	"testing"
 
@@ -43,3 +44,88 @@ func TestStream_CurrentIndex(t *testing.T) {
 	require.Equal(t, []byte("11111"), bs)
 	require.Equal(t, 5, s.CurrentIndex())
 }
+
+func TestStreamReader(t *testing.T) {
+	var s *Stream
+	var data []byte
+	var err error
+
+	s = NewStreamFromReader(bytes.NewReader([]byte("abbcccddddeeeee")))
+	data, err = s.ReadN(1)
+	require.NoError(t, err)
+	require.Equal(t, []byte("a"), data)
+	data, err = s.ReadN(5)
+	require.NoError(t, err)
+	require.Equal(t, []byte("bbccc"), data)
+	data, err = s.PeekN(3)
+	require.NoError(t, err)
+	require.Equal(t, []byte("ddd"), data)
+	data, err = s.PeekN(2)
+	require.NoError(t, err)
+	require.Equal(t, []byte("dd"), data)
+	data, err = s.PeekN(5)
+	require.NoError(t, err)
+	require.Equal(t, []byte("dddde"), data)
+	data, err = s.ReadN(4)
+	require.NoError(t, err)
+	require.Equal(t, []byte("dddd"), data)
+	data, err = s.ReadN(1)
+	require.NoError(t, err)
+	require.Equal(t, []byte("e"), data)
+	data, err = s.ReadN(3)
+	require.NoError(t, err)
+	require.Equal(t, []byte("eee"), data)
+	data, err = s.PeekN(4)
+	require.Error(t, err)
+	data, err = s.PeekN(1)
+	require.NoError(t, err)
+	require.Equal(t, []byte("e"), data)
+	data, err = s.ReadN(2)
+	require.Error(t, err)
+	data, err = s.ReadN(1)
+	require.NoError(t, err)
+	require.Equal(t, []byte("e"), data)
+	data, err = s.ReadN(1)
+	require.Error(t, err)
+}
+
+func TestEmptyStream(t *testing.T) {
+	var s *Stream
+	var data []byte
+	var err error
+
+	s = NewStreamFromReader(bytes.NewReader([]byte{}))
+	data, err = s.ReadN(2)
+	require.Error(t, err)
+	require.Nil(t, data)
+
+	data, err = s.PeekN(1)
+	require.Error(t, err)
+
+	s = NewStreamFromReader(bytes.NewReader(nil))
+	data, err = s.ReadN(1)
+	require.Error(t, err)
+}
+
+func TestStreamBuf(t *testing.T) {
+	var s *Stream
+	var data []byte
+	var err error
+
+	s = NewStreamFromReader(bytes.NewReader([]byte("abbcccddddeeeee")))
+	data, err = s.PeekN(20)
+	require.Error(t, err)
+
+	data, err = s.PeekN(3)
+	require.NoError(t, err)
+	require.Equal(t, []byte("abb"), data)
+
+	data, err = s.ReadN(5)
+	require.NoError(t, err)
+	require.Equal(t, []byte("abbcc"), data)
+
+	data, err = s.PeekN(10)
+	require.NoError(t, err)
+	require.Equal(t, []byte("cddddeeeee"), data)
+}
+
